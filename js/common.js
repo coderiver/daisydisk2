f23b8414fc71d83b054266112e3b1649a4143120
etna-scorpio <etna.scorpio@gmail.com>
2014-08-06 10:08:31 +0300

    skrollr is added

--- js/common.js
+++ js/common.js
@@ -1,16 +1,3 @@
 head.ready(function() {
 
-	$('.out').fullpage({
-		anchors:['main', 'reviews', 'homeuse', 'prouse', 'features', 'monitor'],
-		scrollingSpeed: 100,
-		navigation: true,
-		navigationPosition: 'right',
-		navigationTooltips: ['Main', 'Reviews & Advantages', 'Perfect for home use', 'Professional use', 'Features', 'Monitor'],
-		autoScrolling: true,
-		css3: true,
-		touchSensitivity: 5,
-		animateAnchor: true,
-		verticalCentered: false
-	});
-
 });
--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
 head.load("js/lib/jquery.js",
 					"js/lib/cycle.js",
-					"js/lib/jquery.fullPage.min.js",
+					"js/lib/skrollr.js",
 					"js/common.js");
