bf76af862861d00f34774f13e18407bd19357111
etna-scorpio <etna.scorpio@gmail.com>
2014-08-16 13:48:12 +0300

    about page - almost pixel perfect

--- js/common.js
+++ js/common.js
@@ -1,13 +1,76 @@
 head.ready(function() {
-	//for inner pages
+	var onIndexPage = window.location.href.indexOf("index.html") != -1;
 
-	//FAQ
+	if (onIndexPage) {
+		var $doc = $(document),
+			$win = $(window),
+			winW = $win.width(),
+			winH = $win.height(),
+			$body = $('body'),
+			$panels = $('.item-slider'),
+			$nav = $('.nav li');
+			panelsLen = $panels.length,
+			$header = $('.header'),
+			linkScrollDuration = 1100,
+			flag = true,
+			$clientVideo = $('.client-video .jp-jplayer'),
+			isHomepage = $body.hasClass('index');
+
+		function initPanels(){
+			$.each($panels, function(panelIdx){
+				var $panel = $panels.eq(panelIdx);
+					// panelStart = panelIdx == 0 ? '0%' : '100%',
+				$panel.attr('data-'+(panelIdx-1)*100+'p', 'transform:translate(0,100%)') // -1 - in-start
+				$panel.attr('data-'+(panelIdx-0.5)*100+'p', 'transform:translate(0,0%)') // -0.5 - in-end
+				$panel.attr('data-'+(panelIdx)*100+'p', 'transform:translate(0,0%)') // 0 - out-start
+
+				if (panelIdx + 1 < panelsLen){
+					$panel.attr('data-'+(panelIdx+1)*100+'p', 'transform:translate(0,-100%)') // +1 - out-end
+				}
+			})
+		}
+
+		function scrollTo(blockToScroll) {
+			var curPos = $(document).scrollTop(),
+				heightScroll = $(window).height() * $(blockToScroll).index(),
+				scrollTime = Math.abs(heightScroll-curPos)/7.73;
+			s.animateTo(heightScroll);
+		}
+
+		initPanels();
+
+		s = skrollr.init({
+			smoothScrollingDuration:linkScrollDuration,
+			render: function(data){
+				var secMax = data.maxTop/winH,
+					curSecIdx = Math.round(data.curTop / winH),
+					a = ($nav.filter('.active').index()),
+					b = ($nav.eq(curSecIdx).index());
+
+		    	if(flag) {
+					$nav.eq(curSecIdx).addClass('active').siblings().removeClass('active');
+					flag = false;
+				} else {
+					if(a != b) flag = true;
+				}
+			}
+		});
+
+		$(window).resize(function() {
+			$(window).scrollTop($panels.eq($nav.filter('.active').index()).offset().top);
+		});
+
+		$nav.click(function(){
+			scrollTo(this);
+		});
+	} else {
+		//FAQ
 		$('.ask__list-item').click(function(event) {
 			$(this).toggleClass('is-active');
 			$(this).find('.ask__answered').slideToggle('200');
 		});
 
-	//textarea autosize
+		//textarea autosize
 		$(document).on('input.textarea','.autoExpand', function(){
 			var minRows = this.getAttribute('data-min-rows')|0,
 			rows = this.value.split("\n").length;
@@ -15,12 +78,12 @@ head.ready(function() {
 			this.rows = rows < minRows ? minRows : rows;
 		});
 
-	//menu mobile
+		//menu mobile
 		$('.fa-bars').click(function(event) {
 			$(this).parent().find('.menu').slideToggle('fast');
 		});
 
-	//some stuff on textarea/input focus
+		//some stuff on textarea/input focus
 		$('.focus').focusin(function(event) {
 			$(this).prev().addClass('is-active');
 		});
@@ -28,14 +91,7 @@ head.ready(function() {
 			$(this).prev().removeClass('is-active');
 		});
 
-	//store page select
-		$('#worldMap').click(function(event) {
-			$(this).find('.country').toggleClass('is-visible');
-			$(this).toggleClass('is-active');
-			return false;
-		});
-
-		$('#worldMap').find('.country a').click(function(event) {
+		$('#worldMap').find('.country a').click(function() {
 			var countryChange = $(this).data('name');
 			var currencyChange = $(this).data('currency');
 			var priceChange = $(this).data('price');
@@ -47,10 +103,19 @@ head.ready(function() {
 			$('.product__info').find('.price').text(priceChange);
 		});
 
-		$('#countryList').click(function(event) {
+		function showCountryList() {
 			$(this).find('.country').toggleClass('is-visible');
 			$(this).toggleClass('is-active');
 			return false;
+		}
+
+		//store page select
+		$('#worldMap').click(function() {
+			showCountryList();
+		});
+
+		$('#countryList').click(function() {
+			showCountryList();
 		});
 
 		$('#countryList').find('.country a').click(function(event) {
@@ -59,81 +124,5 @@ head.ready(function() {
 
 			$(chosen).text(countryChange);
 		});
-
-	// //for the main page
-	// $(function() {
-	// function sliderNav(){
-	// 	var $doc = $(document),
-	// 		$win = $(window),
-	// 		winW = $win.width(),
-	// 		winH = $win.height(),
-	// 		$body = $('body'),
-	// 		$panels = $('.item-slider'),
-	// 		$nav = $('.nav li');
-	// 		panelsLen = $panels.length,
-	// 		$header = $('.header'),
-	// 		linkScrollDuration = 1100,
-	// 		flag = true,
-	// 		$clientVideo = $('.client-video .jp-jplayer'),
-	// 		isHomepage = $body.hasClass('index');
-
-
-	// 	//////////
-	// 	// funcs
-	// 	/////////
-
-	// 	function initPanels(){
-	// 		$.each($panels, function(panelIdx){
-	// 			var $panel = $panels.eq(panelIdx);
-	// 				// panelStart = panelIdx == 0 ? '0%' : '100%',
-	// 			$panel.attr('data-'+(panelIdx-1)*100+'p', 'transform:translate(0,100%)') // -1 - in-start
-	// 			$panel.attr('data-'+(panelIdx-0.5)*100+'p', 'transform:translate(0,0%)') // -0.5 - in-end
-	// 			$panel.attr('data-'+(panelIdx)*100+'p', 'transform:translate(0,0%)') // 0 - out-start
-
-	// 			if (panelIdx + 1 < panelsLen){
-	// 				$panel.attr('data-'+(panelIdx+1)*100+'p', 'transform:translate(0,-100%)') // +1 - out-end
-	// 			}
-	// 		})
-	// 	}
-
-	// 	function scrollTo(blockToScroll) {
-	// 		var curPos = $(document).scrollTop(),
-	// 			heightScroll = $(window).height() * $(blockToScroll).index(),
-	// 			scrollTime = Math.abs(heightScroll-curPos)/7.73;
-	// 		s.animateTo(heightScroll);
-	// 	}
-
-	// 	initPanels()
-
-	// 	s = skrollr.init({
-	// 		smoothScrollingDuration:linkScrollDuration,
-	// 		render: function(data){
-	// 			var secMax = data.maxTop/winH,
-	// 				curSecIdx = Math.round(data.curTop / winH),
-	// 				a = ($nav.filter('.active').index()),
-	// 				b = ($nav.eq(curSecIdx).index());
-
-	// 	    	if(flag){
-	// 				$nav.eq(curSecIdx).addClass('active').siblings().removeClass('active');
-	// 			flag = false;
-	// 		} else{
-	// 			if(a != b) flag = true;
-	// 		}
-	// 	}
-	// });
-
-	// $(window).resize(function() {
-	// 	$(window).scrollTop($panels.eq($nav.filter('.active').index()).offset().top);
-	// });
-	// $nav.click(function(){
-	// 	scrollTo(this);
-	// });
-
-	//  }
-
-	//  $(window).ready(function(){
-	// sliderNav();
-	//  });
-	//   });
-
+	}
 });
