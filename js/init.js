de917c8d417dccde74ea45a56d890a3e70edcc5f
etna-scorpio <etna.scorpio@gmail.com>
2014-09-30 11:34:32 +0300

    slider fx

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 head.load("js/lib/jquery.js",
+			"js/common.js",
 			"js/lib/cycle.js",
 			"js/lib/cycle-swipe.js",
-			"js/lib/jquery.scrollTo.js",
-			"js/common.js");
+			"js/lib/jquery.scrollTo.js"
+			);
